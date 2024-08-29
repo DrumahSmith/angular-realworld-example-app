3e884b55e4043a5cb11bdc75ddf49599a2a11a22
DrumahSmith <christopher.drummer.smith@gmail.com>
2024-08-29 15:27:36 +1200

    add init subscribers management

---
+++ src/app/features/newsletter/pages/subscribers/subscribers.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-subscribers",
+  standalone: true,
+  imports: [],
+  templateUrl: "./subscribers.component.html",
+  styleUrl: "./subscribers.component.css",
+})
+export class SubscribersComponent implements OnInit {
+  ngOnInit(): void {}
+}
