f4a2a9c7fa0da0c6563c6ff76a0ffdf1a0b2cede
DrumahSmith <christopher.drummer.smith@gmail.com>
2024-08-29 15:35:45 +1200

    wire up get subscribers

--- src/app/features/newsletter/pages/subscribers/subscribers.component.ts
+++ src/app/features/newsletter/pages/subscribers/subscribers.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
+import { Subscriber } from "../../models/subscriber.model";
+import { NgForOf } from "@angular/common";
+import { SubscribersService } from "../../services/subscribers.service";
+import { IfAuthenticatedDirective } from "src/app/core/auth/if-authenticated.directive";
 
 @Component({
   selector: "app-subscribers",
   standalone: true,
-  imports: [],
+  imports: [NgForOf, IfAuthenticatedDirective],
   templateUrl: "./subscribers.component.html",
   styleUrl: "./subscribers.component.css",
 })
-export class SubscribersComponent implements OnInit {
+export default class SubscribersComponent implements OnInit {
+  subscribers: Subscriber[] = [];
+
+  constructor(private readonly subscribersService: SubscribersService) {
+    this.getSubscribers();
+  }
+
   ngOnInit(): void {}
+
+  private getSubscribers() {
+    this.subscribersService.getSubscribers().subscribe((subscribers) => {
+      this.subscribers = subscribers;
+    });
+  }
 }
