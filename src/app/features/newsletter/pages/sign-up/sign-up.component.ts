e745029f6d4698703f46057f3f13754dc78055ba
DrumahSmith <christopher.drummer.smith@gmail.com>
2024-08-30 13:27:30 +1200

    correct form validation

--- src/app/features/newsletter/pages/sign-up/sign-up.component.ts
+++ src/app/features/newsletter/pages/sign-up/sign-up.component.ts
@@ -3,6 +3,7 @@ import { SubscribersService } from "../../services/subscribers.service";
 import { ActivatedRoute } from "@angular/router";
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
@@ -23,6 +24,7 @@ export default class SignUpComponent implements OnInit {
   username: string = "";
   submitted = false;
   subscriberForm!: FormGroup;
+  subscriberEmail: string = "";
 
   constructor(
     private readonly subscribersService: SubscribersService,
@@ -31,7 +33,7 @@ export default class SignUpComponent implements OnInit {
     private readonly sendMailService: SendmailService,
   ) {
     this.subscriberForm = this.formBuilder.group({
-      email: ["", [Validators.required, Validators.email]],
+      email: new FormControl(null, [Validators.required, Validators.email]),
     });
   }
 
@@ -39,34 +41,32 @@ export default class SignUpComponent implements OnInit {
     this.username = this.activatedRoute.snapshot.params["username"];
   }
 
-  private addSubscriber(email: string) {
+  public onSubmit() {
+    this.submitted = true;
+    console.log("submitted");
+
+    if (this.subscriberForm.invalid) {
+      return;
+    }
+    // return;
+    const email = this.subscriberForm.value.email;
     this.subscribersService
       .addSubscriber(this.username, email)
       .subscribe((subscriber: Subscriber) => {
         const subscribedEmail = subscriber?.email;
+        console.log(subscribedEmail);
         if (!subscribedEmail) {
           // error
           alert("unable to subscribe at this time");
+          return;
         }
         // send email confirmation
         /*
-        ideally we would send the confirmation as a queued task from our backend
-        But as we don't have control over the backend
-       */
+      ideally we would send the confirmation as a queued task from our backend
+      But as we don't have control over the backend
+     */
         this.sendMailService.sendmail(subscribedEmail);
       });
-  }
-
-  public onSubmit() {
-    this.submitted = true;
-    console.log("submitted");
-    if (this.subscriberForm.invalid) {
-      return;
-    }
-    // console.log(this.subscriberForm.value.email);
-    // return;
-
-    this.addSubscriber(this.subscriberForm.value.email);
   }
 
   onReset() {
